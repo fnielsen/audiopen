8e1570224ab21a506a19fad9fc53ba6fc070590d
Finn Aarup Nielsen <faan@dtu.dk>
2016-07-06 11:07:53 +0200

    Implement download function.

--- audiopen/gendervoice.py
+++ audiopen/gendervoice.py
@@ -1,10 +1,13 @@
-"""Gendervoice.
+"""Gendervoice."""
 
-"""
+import errno
+
+from os import makedirs
+from os.path import exists, expanduser, join
 
 from string import split
 
-from os.path import expanduser, join
+from urllib2 import urlopen
 
 from urlparse import urlsplit
 
@@ -30,6 +33,8 @@ select ?person ?personLabel ?audio ?gender ?genderLabel where {
 def query_metadata():
     """Query Wikidata for metadata.
 
+    This function will use the query.wikidata.org SPARQL service.
+
     Returns
     -------
     df : pandas.DataFrame
@@ -42,8 +47,28 @@ def query_metadata():
     return df
 
 
+def field_to_value(field):
+    """Convert sparql return type to string.
+
+    Parameters
+    ----------
+    field : sparql.IRI
+        SPARQL return element
+
+    Returns
+    -------
+    str : str
+        String if 'value' attribute is present
+
+    """
+    if hasattr(field, 'value'):
+        return field.value
+    else:
+        return field
+
+
 def link_to_filename(link):
-    """Convert Wikimedia Commons link to filename.
+    r"""Convert Wikimedia Commons link to filename.
 
     Parameters
     ----------
@@ -54,7 +79,7 @@ def link_to_filename(link):
     -------
     filename : str
         Filename part of the link
-    
+
     Examples
     --------
     >>> link = "http://commons.wikimedia.org/wiki/" \
@@ -63,5 +88,64 @@ def link_to_filename(link):
     'Sound.flac'
 
     """
+    link = field_to_value(link)   # possible conversion for sparql.IRI
     filename = split(urlsplit(link).path, '/')[-1]
     return filename
+
+
+def download_one(link, directory=DATA_DIRECTORY):
+    """Download a specified Wikimedia Commons file.
+
+    Parameters
+    ----------
+    link : str
+        String with IRI
+    directory : str
+        Local directory where the audio files are to be stored.
+
+    """
+    link = field_to_value(link)
+    filename = link_to_filename(link)
+    local_filename = join(directory, filename)
+    if not exists(local_filename):
+        # Buffered output
+        # https://gist.github.com/hughdbrown/c145b8385a2afa6570e2
+        buffer = urlopen(link)
+        with open(local_filename, 'wb') as output:
+            while True:
+                data = buffer.read(4096)
+                if data:
+                    # TODO: should use temporary file
+                    output.write(data)
+                else:
+                    break
+
+
+def download_all(metadata, directory=DATA_DIRECTORY):
+    """Download data from Wikimedia Commons.
+
+    This will download all audio files mentioned in the metadata,
+    if they are not already downloaded.
+
+    Parameters
+    ----------
+    metadata : pandas.DataFrame
+        Dataframe with metadata
+    directory : str
+        Local directory where the audio files are to be stored.
+
+    Examples
+    --------
+    >>> download_all()
+
+    """
+    # Make directory if not exist
+    try:
+        makedirs(directory)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
+
+    # Download audio files iteratively
+    for remote_filename in metadata.audio:
+        download_one(remote_filename, directory=directory)
