9fa28f9dc7a86eeb64e9756aa83d61c777d70590
fnielsen <fn@imm.dtu.dk>
2016-07-13 22:49:29 +0200

    Change to function implementation, plots

--- examples/gender_pitches.py
+++ examples/gender_pitches.py
@@ -5,17 +5,61 @@ Shows median pitch for the files in the gendervoice dataset.
 
 """
 
+import wave
+
+import pandas as pd
+
 import numpy as np
+
+import seaborn as sns
+
+from matplotlib.pyplot import *
 
 import audiopen.gendervoice
 
+# Load metadata, and the data if that is not already downloaded
+metadata = audiopen.gendervoice.load_metadata()
+audiopen.gendervoice.download_all(metadata)
 
-metadata = audiopen.gendervoice.query_metadata()
-# audiopen.gendervoice.download_all(metadata)
+# Iterate over all audio files and get pitch.
+filenames = audiopen.gendervoice.get_filenames(metadata)
+pitches_for_all = []
+for filename, name, gender in zip(
+        filenames, metadata.personLabel, metadata.genderLabel):
+    try:
+        samples = audiopen.gendervoice.get_samples_mono_11025(filename)
+    except wave.Error:
+        continue
+    print(filename)
+    pitches_for_person = audiopen.gendervoice.get_pitches(samples)
+    pitches_for_all.append((pitches_for_person, gender, name, filename))
 
-samples = audiopen.gendervoice.iter_samples_mono_11025(
-    metadata, yieldgender=True)
-for sample, gender in samples:
-    pitches = audiopen.gendervoice.iter_samples_to_pitches(sample)
-    pitch = np.median([pitch for pitch in pitches])
-    print("{} {}".format(pitch, gender))
+
+# Dataframe representation
+pitches = pd.DataFrame(pitches_for_all,
+                       columns=['pitches', 'gender', 'name', 'filename'])
+
+
+# Various pitch aggregation functions
+def median(pitches):
+    """Return median of pitches."""
+    return np.median(pitches[:, 0])
+
+
+def confidence_median(pitches):
+    """Return median pitch from pitches with top confience."""
+    confidence = pitches[:, 1]
+    indices = np.argsort(confidence)
+    return np.median(pitches[indices[-min(10, len(indices)):], 0])
+
+
+# Apply the pitch aggregation functions
+pitches['median_pitch'] = pitches.pitches.apply(median)
+pitches['confidence_pitch'] = pitches.pitches.apply(confidence_median)
+
+
+# Stripplot of male vs. female pitches
+figure()
+sns.stripplot(x=pitches.gender, y=pitches.confidence_pitch, jitter=.1)
+ylim(50, 300)
+show()
