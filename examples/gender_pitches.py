bcf62cc6dcf2a5ca3a40a336062f5851cea5c35b
Finn Aarup Nielsen <faan@dtu.dk>
2016-07-13 02:03:41 +0200

    Fix missing import

--- examples/gender_pitches.py
+++ examples/gender_pitches.py
@@ -1,6 +1,21 @@
+#!/usr/bin/env python
+"""gender_pitches.py
+
+Shows median pitch for the files in the gendervoice dataset.
+
+"""
+
+import numpy as np
+
 import audiopen.gendervoice
 
-samples = audiopen.gendervoice.samples_mono_11025(metadata, yieldgender=True)
+
+metadata = audiopen.gendervoice.query_metadata()
+# audiopen.gendervoice.download_all(metadata)
+
+samples = audiopen.gendervoice.iter_samples_mono_11025(
+    metadata, yieldgender=True)
 for sample, gender in samples:
-    pitch = np.median([pitch for pitch in audiopen.gendervoice.samples_to_pitches(sample)])
+    pitches = audiopen.gendervoice.iter_samples_to_pitches(sample)
+    pitch = np.median([pitch for pitch in pitches])
     print("{} {}".format(pitch, gender))
