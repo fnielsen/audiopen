f1d49d478060d5c682d412d8885d77b019ec8abc
fnielsen <fn@imm.dtu.dk>
2016-07-14 16:40:49 +0200

    Add print of results

--- examples/gender_pitches.py
+++ examples/gender_pitches.py
@@ -5,6 +5,8 @@ Shows median pitch for the files in the gendervoice dataset.
 
 """
 
+from __future__ import division, print_function
+
 import wave
 
 import pandas as pd
@@ -17,12 +19,14 @@ from matplotlib.pyplot import *
 
 import audiopen.gendervoice
 
+
 # Load metadata, and the data if that is not already downloaded
 metadata = audiopen.gendervoice.load_metadata()
 audiopen.gendervoice.download_all(metadata)
 
 # Iterate over all audio files and get pitch.
 filenames = audiopen.gendervoice.get_filenames(metadata)
+
 pitches_for_all = []
 for filename, name, gender in zip(
         filenames, metadata.personLabel, metadata.genderLabel):
@@ -63,3 +67,14 @@ figure()
 sns.stripplot(x=pitches.gender, y=pitches.confidence_pitch, jitter=.1)
 ylim(50, 300)
 show()
+
+
+for filename, name, gender in zip(
+        filenames, metadata.personLabel, metadata.genderLabel):
+    try:
+        detected_gender = audiopen.gendervoice.detect_gender(filename)
+    except wave.Error:
+        continue
+    print("Detected: {:6s} - Actual: {:6s} - {} - {}".format(
+        detected_gender, gender, name, filename))
+
