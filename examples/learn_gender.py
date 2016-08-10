9140fe4e72b6d69e872a85b7b591b0284634ccf0
fnielsen <fn@imm.dtu.dk>
2016-08-10 22:54:49 +0200

    Fix import error and add printout

--- examples/learn_gender.py
+++ examples/learn_gender.py
@@ -3,24 +3,21 @@
 
 Learn gender for the gendervoice dataset.
 
+This script is work in progress. It does not perform well.
+
 """
 
 from __future__ import division, print_function
 
+import wave
+
+import numpy as np
+
 from keras.models import Sequential
 from keras.layers import Dense, Activation
+from keras.optimizers import SGD
 
 import audiopen.gendervoice
-
-
-# Keras deep learning
-model = Sequential()
-model.add(Dense(output_dim=64, input_dim=15))
-model.add(Activation("relu"))
-model.add(Dense(output_dim=2))
-model.add(Activation("softmax"))
-model.compile(loss='categorical_crossentropy',
-              optimizer='sgd', metrics=['accuracy'])
 
 
 # Load metadata, and the data if that is not already downloaded
@@ -30,18 +27,32 @@ audiopen.gendervoice.download_all(metadata)
 feature_extractor = audiopen.gendervoice.FeatureExtractor()
 
 
-# Iterate over all audio files and get pitch.
+# Iterate over all audio files
 filenames = audiopen.gendervoice.get_filenames(metadata)
 
-pitches_for_all = []
+
+# Keras deep learning
+model = Sequential()
+model.add(Dense(output_dim=2, input_dim=15))
+model.add(Activation("tanh"))
+model.add(Dense(output_dim=2))
+model.add(Activation("softmax"))
+model.compile(loss='categorical_crossentropy',
+              optimizer='sgd', metrics=['accuracy'])
+
+
 for filename, name, gender in zip(
         filenames, metadata.personLabel, metadata.genderLabel):
     try:
-        samples = audiopen.gendervoice.get_samples_mono_11025(filename)
+        samples = audiopen.gendervoice.get_samples_mono_22050(filename)
     except wave.Error:
         continue
-    print(filename)
-    feature_set = np.array([features
-                            for features in audiopen.gendervoice.iter_samples_to_features(samples)])
+    feature_set = np.array(
+        [features
+         for features in audiopen.gendervoice.iter_samples_to_features(samples, sample_rate=22050)])
+    proba = model.predict_proba(feature_set, batch_size=32, verbose=0)
+    probability_of_female = np.mean(proba, axis=0)[0]
     Y = np.repeat([[1, 0]] if gender == 'male' else [[0, 1]], feature_set.shape[0], axis=0)
-    model.train_on_batch(feature_set, Y)
+    print("Training on {} with gender {} with sample of size {}. Female estimate: {}".format(
+        name, gender, Y.shape[0], probability_of_female))
+    _ = model.train_on_batch(feature_set, Y)
